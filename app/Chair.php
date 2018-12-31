9b350e12534da9b2c49832c3dfda9047d7ed21c4
Spencer Stewart <spencer@spencer.tech>
2018-12-31 08:12:22 -0800

    Write function to get chair status for summit

--- app/Chair.php
+++ app/Chair.php
@@ -10,4 +10,31 @@ class Chair extends Model
     {
         return $this->belongsTo('App\Resort');
     }
+
+    /**
+     * Returns array of of chair statuses for a particular resort
+     * Takes input of the XML url and the re
+     */
+    public static function getSummitChairStatus($url = 'https://www.bigbearmountainresort.com/Feeds/Xml/Mtn/v2.ashx?Resort=SnowSummit', $resortId = 1)
+    {
+        $chairStatus = [];
+        $resortStatus = new \SimpleXMLElement(file_get_contents($url));
+        $lifts = $resortStatus->facilities->facility[1]->lifts->lift;
+        foreach ($lifts as $lift) {
+            // Filter out Magic Carpets and only get Chair status
+            if (!preg_match('/Chair/', $lift['name'])) {
+                continue;
+            } else {
+                preg_match('/[0-9]+/', $lift['name'], $number); // Get the Chair number
+                $chairStatus[] = [
+                    'name' => $lift['name'],
+                    'number' => $number,
+                    'status' => $lift['status'],
+                    'areaName' => $lift['areaName'],
+                    'resort_id' => 1
+                ];
+            }
+        }
+        return $chairStatus;
+    }
 }
