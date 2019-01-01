f486680d77235db36c61d3e9d20fa67c969f963c
Spencer Stewart <spencer@spencer.tech>
2019-01-01 07:36:35 -0800

    Refactor API for chairs

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,23 @@ Route::get('{resort}', function ($resort) {
     return \App\Resort::where('slug', $resort)->with('chairs')->first();
 });
 
+Route::get('{resort}/chairs', function (App\Resort $resort) {
+    return $resort->chairs()->get();
+});
+
+Route::get('{resort}/chairs/open', function (App\Resort $resort) {
+    return $resort->chairs()->where('status', 'Open')->get();
+});
+
+Route::get('{resort}/chairs/closed', function (App\Resort $resort) {
+    return $resort->chairs()->where('status', '!=', 'Open')->get();
+});
+
 Route::get('{resort}/chairs/{chair_number}', function (App\Resort $resort, $chair_number) {
     return $resort->chairs()->where('number', $chair_number)->first();
     // return $chair_number;
 });
 
-Route::get('{resort}/chairs', function (App\Resort $resort) {
-    $open_chairs = $resort->chairs()->where('status', 'Open')->get();
-    $closed_chairs = $resort->chairs()->where('status', '!=', 'Open')->get();
-    $chairs[] = ['open' => 2, 'closed' => 9];
-    $chair_summary = [
-        'numberOpen' => $open_chairs->count(),
-        'numberClosed' => $closed_chairs->count(),
-        'openChairs' => $open_chairs->pluck('number'),
-        'closedChairs' =>$closed_chairs->pluck('number')
-    ];
-    return $chair_summary;
-});
 
 Route::get('{resort}/trails', function(App\Resort $resort) {
     $trails = $resort->trails;
