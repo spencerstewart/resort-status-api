327469a8ea5c8a99abe0ace6ed04c15817e6e4a8
Spencer Stewart <spencer@spencer.tech>
2019-01-02 17:22:12 -0800

    Add api-endpoint column for resorts

--- database/migrations/2018_12_28_040913_create_resorts_table.php
+++ database/migrations/2018_12_28_040913_create_resorts_table.php
@@ -17,6 +17,7 @@ class CreateResortsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug')->unique();
+            $table->string('api-endpoint');
             $table->timestamps();
         });
     }
