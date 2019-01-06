62ac27bb428c1ea3a04d78e60ef03ff7e4222de2
Spencer Stewart <spencer@spencer.tech>
2019-01-06 05:00:31 -0800

    Attempt to check hashes

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use function GuzzleHttp\json_decode;
 
 
 class WebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        Log::error($request->header('authorization'));
+        $security_token = "KaLPviRJMnxqtDnVyStGFotWCPixu2OEjDnP+lZqp88=";
+        Log::error(utf8_encode($request->getContent()));
+        Log::error(json_encode($request->getContent()));
+        Log::error(json_encode($request->all()));
+
+        $hash_expected = $request->header('authorization');
+        $hash_received1 = "HMAC " . base64_encode(hash_hmac('sha256', utf8_encode($request->getContent()), $security_token, TRUE));
+        $hash_received2 = "HMAC " . base64_encode(hash_hmac('sha256', json_encode($request->all()), $security_token, TRUE));
+
+        Log::error("Hash expected: " . $hash_expected);
+        Log::error("Hash received 1: " . $hash_received1);
+        Log::error("Hash calculated 2: " . $hash_received2);
+
+
+        if (hash_equals($hash_expected, $hash_received1)) {
+            // Hashes match
+        }
+
+
         return [
             'type' => 'message',
             'text' => 'You said ' . $request->input('text')
