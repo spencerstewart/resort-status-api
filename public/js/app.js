ce5ce9b69cf9b9f47a9c434a1776fd0715e40a78
Spencer Stewart <spencer@spencer.tech>
2019-01-07 20:24:22 -0800

    Changes to scss for card formatting

--- public/js/app.js
+++ public/js/app.js
@@ -48111,8 +48111,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\sstewart\Code\resort-status-api\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\sstewart\Code\resort-status-api\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/spencer/Code/ss/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/spencer/Code/ss/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -8,9 +8,20 @@
     <div class="row">
         <div class="col-md">
             <h1>Welcome to the Resort Status Page</h1>
-            <p>
-                Information on which venues are open and etc will be published here.
-            </p>
+            <h4>Message of the day</h4>
+            @foreach ($todays_messages as $message)
+                <div class="row">
+                    <div class="col-md">
+                        <div class="card" style="">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $message->from }}</h5>
+                                <p class="card-text">{{ $message->message }}</p>
+                                <p class="card-text">{{ \Carbon\Carbon::parse($message->created_at)->diffForHumans() }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
