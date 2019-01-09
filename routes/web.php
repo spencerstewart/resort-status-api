7b379037ecbbf8bd0396d40851b7e1868c0cffa2
Spencer Stewart <sstewart@bbmr.com>
2019-01-09 15:33:29 -0800

    Add api prefixed routes

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,75 @@ Route::post('motd', 'WebhookController@handleMotd');
 Route::get('webhook', 'WebhookController@index');
 
 
-// Route::get('/snowsummit/chair/1', function() {
-//     return \App\Chair::first();
-// });
+/**
+ * Prefixed routes with api
+ */
+
+Route::prefix('api')->group(function() {
+    Route::get('{resort}', function ($resort) {
+        return \App\Resort::where('slug', $resort)->with('chairs')->first();
+    });
+    
+    Route::get('{resort}/chairs', function (App\Resort $resort) {
+        return $resort->chairs()->get();
+    });
+    
+    Route::get('{resort}/chairs/open', function (App\Resort $resort) {
+        return $resort->chairs()->where('status', 'Open')->get();
+    });
+    
+    Route::get('{resort}/chairs/closed', function (App\Resort $resort) {
+        return $resort->chairs()->where('status', '!=', 'Open')->get();
+    });
+    
+    Route::get('{resort}/chairs/{chair_number}', function (App\Resort $resort, $chair_number) {
+        return $resort->chairs()->where('number', $chair_number)->first();
+        // return $chair_number;
+    });
+    
+    
+    Route::get('{resort}/trails', function(App\Resort $resort) {
+        return $resort->trails;
+    });
+    
+    Route::get('{resort}/trails/open', function(App\Resort $resort) {
+        return $resort->trails()->where('status', 'Open')->get();
+    });
+    
+    Route::get('{resort}/trails/{trail}', function(App\Resort $resort, $trail) {
+        return $resort->trails()->where('slug', $trail)->first();
+    });
+    
+    Route::get('{resort}/trails/difficulty/{difficulty}', function(App\Resort $resort, $difficulty) {
+        switch ($difficulty) {
+            case 'easiest':
+                $difficulty = 'Easiest';
+                break;
+            case 'moderate':
+                $difficulty = 'Slightly More Difficult';
+                break;
+            case 'difficult':
+                $difficulty = 'Difficult';
+                break;
+            case 'expert':
+                $difficulty = 'Most Difficult';
+                break;
+            default:
+                break;
+        }
+        return $resort->trails()->where('status', 'Open')->where('difficulty', $difficulty)->get();
+    });
+    
+    Route::get('{resort}/api/lessons', function(App\Resort $resort) {
+        return $resort->lessons;
+    });
+    
+    Route::get('{resort}/lessons', 'LessonController@index');
+});
+
+/**
+ * Old routes below
+ */
 
 Route::get('{resort}', function ($resort) {
     return \App\Resort::where('slug', $resort)->with('chairs')->first();
@@ -81,4 +147,3 @@ Route::get('{resort}/api/lessons', function(App\Resort $resort) {
 });
 
 Route::get('{resort}/lessons', 'LessonController@index');
-
