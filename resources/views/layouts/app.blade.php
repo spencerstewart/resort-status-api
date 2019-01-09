d3c0687bb7b9dd63fa7496c7d9596c5efeeed1d7
Spencer Stewart <sstewart@bbmr.com>
2019-01-09 13:16:47 -0800

    Change location of vue script to end of HTML body

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ env('APP_NAME')}}</title>
 
     <link rel="stylesheet" href="css/app.css">
-    <script src="js/app.js"></script>
 </head>
 <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">@yield('content')</div>
-                <div class="col-md-4">@yield('sidebar')</div>
-            </div>
+    <div class="container" id="app">
+        <div class="row">
+            <div class="col-md-8">@yield('content')</div>
+            <div class="col-md-4">@yield('sidebar')</div>
         </div>
+    </div>
+
+    <script src="js/app.js"></script>
+
 </body>
 </html>
