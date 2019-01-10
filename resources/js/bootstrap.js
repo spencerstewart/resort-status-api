59e3576e5bc146350d667d4a6e3beb7256d673fe
Spencer Stewart <spencer@spencer.tech>
2019-01-10 11:17:37 -0800

    Update hardcoded pusher app key in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,7 +50,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'c6e11d82815097d78073',
+    key: '65439e4db68f54750b2a',
     cluster: 'us2',
     // key: process.env.MIX_PUSHER_APP_KEY,
     // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
