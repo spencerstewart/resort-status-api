2eefd31fc2845602d8255f5ca07125c7ab1fcbf5
Spencer Stewart <spencer@spencer.tech>
2019-01-11 11:12:11 -0800

    Change Heading of home page

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
         <div class="col-md">
-            <h1>Welcome to the Resort Status Page</h1>
+            <h1>Welcome to the BBMR Status Page</h1>
             <h4>Message of the day - {{ \Carbon\Carbon::today()->toFormattedDateString() }}</h4>
             <div class="row">
                 <div class="col">
