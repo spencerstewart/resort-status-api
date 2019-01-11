f90801f5d034ef59c14480e5be0d9ae282a48306
Spencer Stewart <spencer@spencer.tech>
2019-01-10 20:06:55 -0800

    Add firebase vue component

---
+++ resources/js/components/Firebase.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-show="notificationsEnabled">
+    <h1>Notifications are enabled.</h1>
+  </div>
+</template>
+
+<script>
+    // import Echo from 'laravel-echo';
+    // import Pusher from 'pusher-js';
+    export default {
+        mounted() {
+            
+        },
+        data() {
+            return {
+                notificationsEnabled: false
+            }
+        },
+        created() {
+            const messaging = firebase.messaging();
+
+            
+            
+            messaging.requestPermission()
+            .then(function() {
+              console.log('[Firebase Messaging] Notification permission granted');
+              return messaging.getToken();
+            })
+            .then(function(token) {
+              console.log(token);
+            })
+            .catch(function(err) {
+              console.log('[Firebase Messaging: Error]', err);
+            })
+
+        }
+    }
+</script>
